cd229643471103e5a77f101b580bab35489f63d4
miyashiiii <yosuke.miyashita@gmail.com>
2021-10-29 01:46:55 +0900

    Add ABC216-C

---
+++ ABC216/C.py
@@ -0,0 +1,23 @@
+"""
+https://atcoder.jp/contests/abc216/tasks/abc216_c5
+
+5
+"""
+
+
+def main():
+    n = int(input())
+    ans = ""
+    while n > 0:
+        if n % 2 == 0:
+            n //= 2
+            ans = "B" + ans
+        else:
+            n -= 1
+            ans = "A" + ans
+
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()
